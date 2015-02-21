e675ca4d4deeed7901f8978458b73b0279c95d37
Peter Watts <pwattsmail@gmail.com>
2015-02-21 18:00:40 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 $( document ).ready(function() {
   $("h2").each(function(i, h2){
-    $("#sidebar").append("<div>"+$(h2).text()+"</div>");
+    $("#sidebar").append("<div onclick="scrollTo(i)">"+$(h2).text()+"</div>");
   });
 });
+function scrollTo(i) {
+  $('html,body').animate({
+    $($("h2")[i]).offset().top
+  }, 1000);
+}
