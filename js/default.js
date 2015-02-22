23363977111e2ff5d39e58c57d253d5bfc533b01
Peter Watts <pwattsmail@gmail.com>
2015-02-21 21:28:34 -0500

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
   $("h2").each(function(i, h2){
-    $("#sidebar").append('<li onclick="scrollTo('+i+')">'+$(h2).text()+'</li>');
+    $("#sections").append('<li onclick="scrollTo('+i+')">'+$(h2).text()+'</li>');
   });
 });
 function scrollTo(i) {
